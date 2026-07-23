b174de540f7daf60d253a33a044a95ae5b38c5df
diegogs92 <dgarciasantillan@gmail.com>
2026-07-23 17:52:21 -0300

    fix(web): actualiza numero de WhatsApp del concesionario

--- web/lib/site.js
+++ web/lib/site.js
@@ -1,7 +1,7 @@
 // Identidad y contacto reales de ICY Automotores (Yerba Buena, Tucumán).
 export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
-export const WHATSAPP_NUMBER = '5493815540754'
-export const WHATSAPP_DISPLAY = '+54 9 381 554-0754'
+export const WHATSAPP_NUMBER = '543812482424'
+export const WHATSAPP_DISPLAY = '+54 381 248-2424'
 export const EMAIL = 'imanolcardenasypa@gmail.com'
 export const INSTAGRAM_HANDLE = '@icy_automotores_ok'
 export const INSTAGRAM_URL = 'https://instagram.com/icy_automotores_ok'
